31113ba2ba91938a42591968112d3c7852f55834
Nesarul Hoque <nesarul@yahoo.com>
2022-01-11 12:33:10 +0600

    Add files via upload

---
+++ admin/function.php
@@ -0,0 +1,3 @@
+<?php
+    define('DS',DIRECTORY_SEPARATOR);
+    define('DR',$_SERVER['DOCUMENT_ROOT'].DS.'expense'.DS);
---
+++ admin/login/login.php
@@ -0,0 +1,10 @@
+<?php 
+    require_once '../function.php';
+    require_once DR.'inc'.DS.'header.php';
+?>
+    <h1>بسم الله الر حمن الر حيم</h1>
+    <h2>Called Me</h2>
+    
+<?php 
+    require_once DR.'inc'.DS.'footer.php';
+?>
